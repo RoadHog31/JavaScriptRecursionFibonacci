6c285d5b0add03dbc9b208c69c43a98ce791d56b
Your Name <you@example.com>
2022-06-22 08:24:20 +0100

    Additional sequence comments added.

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ F(0) = 0
 F(1) = 1
 F(n) = F(n-1) + F(n-2)
 
+In mathematics, the Fibonacci numbers, commonly denoted Fₙ, form a sequence, the Fibonacci sequence, in which each number is the sum of the two preceding ones. The sequence commonly starts from 0 and 1, although some authors omit the initial terms and start the sequence from 1 and 1 or from 1 and 2.
+
 What are the first 10 Fibonacci numbers?
 The list of first 20 terms in the Fibonacci Sequence is: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181.
 */
