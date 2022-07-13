4a9949be1602b4ee5949bab1e00015963e80d2bf
Your Name <you@example.com>
2022-07-13 19:33:25 +0100

    app2.js added with anothe example.

---
+++ app2.js
@@ -0,0 +1,29 @@
+//Using recursive functions (Example):
+
+// Suppose that you need to develop a function that counts down from a specified number to 1.
+
+// For example, to count down from 5 to 1:
+
+
+function countDown(number) {
+
+    console.log(number);
+
+    let nextNumber = number - 1;
+
+    // In each iteration, the number value is decreased by 1 and the function countDown() is called until the number is positive.
+    //  Here, newNumber > 0 is the base condition.
+
+    //This is the base condition or level. 
+    if (nextNumber > 0) {
+        
+        //Recursion function calls itself.
+        // When the number reaches 0, the base condition is met, and the function is not called anymore. Otherwise, it will call itself indefinitely.
+        countDown(nextNumber); 
+        
+    } 
+    
+}
+
+countDown(10);
+
